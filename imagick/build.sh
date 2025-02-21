d5f2fa986120536e96e92d848c10cad539273032
Luffy <52o@qq52o.cn>
2025-02-21 11:01:48 +0800

    Update version

--- imagick/build.sh
+++ imagick/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-docker-compose build imagick
+docker compose build imagick
 docker run --name imagick build/imagick
 docker cp imagick:/opt/www/modules/imagick.so imagick-${PHP_VERSION}-alpine-v${ALPINE_VERSION}.so
 docker rm imagick
